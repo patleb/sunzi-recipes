9a1d153bc0414f7dc85bdd7e2025871eed289a46
Bryan Powell <bryan@metabahn.com>
2014-11-05 21:23:40 +0000

    Only setup private ip once

--- postgresql-9.3.sh
+++ postgresql-9.3.sh
@@ -7,22 +7,22 @@ else
   apt-get update
 
   apt-get -y install postgresql-9.3 postgresql-server-dev-9.3 postgresql-contrib-9.3
-fi
+  
+  private_ip=$(cat attributes/private_ip)
 
-private_ip=$(cat attributes/private_ip)
+  if [ ! -z "$private_ip" ]; then
+    # bind on the private ip
+    file='/etc/postgresql/9.3/main/postgresql.conf'
+    grep -q "^listen_addresses =" $file && sed "s/^listen_addresses = .*/listen_addresses = '$private_ip'/" -i $file || 
+      sed "$ a\listen_addresses = '$private_ip'" -i $file
 
-if [ ! -z "$private_ip" ]; then
-  # bind on the private ip
-  file='/etc/postgresql/9.3/main/postgresql.conf'
-  grep -q "^listen_addresses =" $file && sed "s/^listen_addresses = .*/listen_addresses = '$private_ip'/" -i $file || 
-    sed "$ a\listen_addresses = '$private_ip'" -i $file
+    # allow access
+    file='/etc/postgresql/9.3/main/pg_hba.conf'
+    conf='host    all             all             all                     md5'
+    if [ ! grep -q "^$conf" $file]; then
+      sed "$ a\$conf" -i $file
+    fi
 
-  # allow access
-  file='/etc/postgresql/9.3/main/pg_hba.conf'
-  conf='host    all             all             all                     md5'
-  if [ ! grep -q "^$conf" $file]; then
-    sed "$ a\$conf" -i $file
+    service postgresql restart
   fi
-
-  service postgresql restart
 fi
