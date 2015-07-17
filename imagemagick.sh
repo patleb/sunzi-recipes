8f81b9bc167de6ac7b8ddf3dfa97a1b46c870407
Bryan Powell <bryan@metabahn.com>
2015-07-17 18:30:12 -0500

    Fix the bloody imagemagick recipe

--- imagemagick.sh
+++ imagemagick.sh
@@ -1,5 +1,5 @@
 if aptitude search '~i ^imagemagick$' | grep -q imagemagick; then
   echo "imagemagick already installed, skipping."
 else
-  apt-get -y install libmagickwand-dev
+  apt-get -y install imagemagick libmagickcore-dev libmagickwand-dev
 fi
