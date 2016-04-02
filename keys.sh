9b2b7427826a4fb3909f5d850952798fb04da4ee
Bryan Powell <bryan@metabahn.com>
2016-04-02 14:13:49 -0500

    fix keys

--- keys.sh
+++ keys.sh
@@ -1,3 +1,11 @@
-<% @attributes.keys.each do |key| %>
-grep -q -F '<%= key %>' /root/.ssh/authorized_keys || echo '<%= key %>' >> /root/.ssh/authorized_keys
-<% end %>
+if aptitude search '~i ^jq$' | grep -q jq; then
+  echo "jq already installed, skipping."
+else
+  sudo apt-get install -y jq
+fi
+
+keys=$(jq -r .[] attributes/keys)
+
+for key in "${keys[@]}"; do
+  grep -q -F "${key}" /root/.ssh/authorized_keys || echo "${key}" >> /root/.ssh/authorized_keys
+done
