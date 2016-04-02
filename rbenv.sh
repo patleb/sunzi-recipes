cf0b08974e763443ec7759b0524750254077f35e
Bryan Powell <bryan@metabahn.com>
2016-04-02 12:48:54 -0500

    Update rbenv

--- rbenv.sh
+++ rbenv.sh
@@ -1,13 +1,48 @@
-if [ -d ~/.rbenv ]; then
-  echo "rbenv already installed, skipping"
+if [ -d /usr/local/rbenv ]; then
+  echo "rbenv already installed"
 else
-  apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
- 
-  # install rbenv
-  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-  echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+  # install packages necessary to install ruby
+  apt-get -y install build-essential
+  apt-get -y install libc6-dev
+  apt-get -y install automake
+  apt-get -y install libtool
 
-  # install ruby-build
-  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+  # https://github.com/sstephenson/ruby-build/issues/119
+  # "It seems your ruby installation is missing psych (for YAML
+  # output). To eliminate this warning, please install libyaml and
+  # reinstall your ruby."
+  apt-get -y install libyaml-dev
+
+  # needed to unpack rubygems
+  apt-get -y install zlib1g
+  apt-get -y install zlib1g-dev
+
+  # openssl support for ruby
+  apt-get -y install openssl
+  apt-get -y install libssl-dev
+
+  # readline for irb and rails console
+  apt-get -y install libreadline-dev
+
+  # for ruby stdlib rexml and nokogiri
+  # http://nokogiri.org/tutorials/installing_nokogiri.html
+  apt-get -y install libxml2-dev
+  apt-get -y install libxslt1-dev
+
+  # better irb support
+  apt-get -y install ncurses-dev
+
+  # for searching packages
+  apt-get -y install pkg-config
+
+  # install rbenv, ruby-build, and auto-rehash plugin
+  git clone https://github.com/sstephenson/rbenv.git /usr/local/rbenv
+  git clone https://github.com/sstephenson/ruby-build.git /usr/local/rbenv/plugins/ruby-build
+
+  echo 'export RBENV_ROOT="/usr/local/rbenv"' >> /etc/profile.d/rbenv.sh
+  echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /etc/profile.d/rbenv.sh
+  echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
 fi
+
+# make rbenv available
+source /etc/profile.d/rbenv.sh
