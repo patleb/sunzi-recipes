4dc26e35d7a6f9a90e32e3ffd8e0a278a63efd3e
Bryan Powell <bryan@metabahn.com>
2016-04-02 20:47:26 -0500

    update redis

--- redis.sh
+++ redis.sh
@@ -3,6 +3,11 @@ if aptitude search '~i ^redis-server$' | grep -q redis-server; then
 else
   apt-add-repository ppa:chris-lea/redis-server
   apt-get update
-  
+
   apt-get -y install redis-server
 fi
+
+rm /etc/redis/redis.conf
+mv files/redis.conf /etc/redis/redis.conf
+
+service redis-server restart
