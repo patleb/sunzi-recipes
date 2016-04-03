6969e7dbf09fd2dd8458a9af91bd4b45817060dd
Bryan Powell <bryan@metabahn.com>
2016-04-03 07:02:07 -0500

    set nginx config permissions

--- nginx-config-default.sh
+++ nginx-config-default.sh
@@ -1,3 +1,5 @@
 rm /etc/nginx/sites-enabled/default
 mv files/nginx-default.conf /etc/nginx/sites-enabled/default
+chown root /etc/nginx/sites-enabled/default
+chgrp root /etc/nginx/sites-enabled/default
 service nginx restart
--- nginx-config.sh
+++ nginx-config.sh
@@ -1,3 +1,5 @@
 rm /etc/nginx/nginx.conf
 mv files/nginx.conf /etc/nginx/nginx.conf
+chown root /etc/nginx/nginx.conf
+chgrp root /etc/nginx/nginx.conf
 service nginx restart
