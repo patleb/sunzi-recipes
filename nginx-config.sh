6750171e5b14f872435b7fab56b7a5913d434adc
Wayne Schroer <wayne@metabahn.com>
2016-04-21 10:12:43 -0500

    Move the logrotate config into the correct place for nginx

--- nginx-config.sh
+++ nginx-config.sh
@@ -1,5 +1,10 @@
 rm /etc/nginx/nginx.conf
+rm /etc/logrotate.d/nginx
+
 mv files/nginx.conf /etc/nginx/nginx.conf
+mv files/nginx-logrotate.conf /etc/logrotate.d/nginx
+
 chown root /etc/nginx/nginx.conf
 chgrp root /etc/nginx/nginx.conf
+
 service nginx restart
