cbe18deb8135c717312924ff4d3647ada668ce8e
Wayne Schroer <wayne@metabahn.com>
2016-04-22 11:16:35 -0500

    Move the config file to the correct location

--- syslog-logrotate.sh
+++ syslog-logrotate.sh
@@ -3,4 +3,4 @@
 # It's safe to just add and not check it if exists because we would
 # want to override this if it is already there.
 
-mv files/syslog-logrotate.conf /etc/logrotate.d/syslog
+mv files/syslog-logrotate.conf /etc/logrotate.d/rsyslog
