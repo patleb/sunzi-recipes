553c358d42f3cfed218b0d258299c1ada2509b4e
Wayne Schroer <wayne@metabahn.com>
2016-04-22 11:00:01 -0500

    Fix the file name

--- apps-logrotate.sh
+++ apps-logrotate.sh
@@ -3,4 +3,4 @@
 # It's safe to just add and not check it if exists because we would
 # want to override this if it is already there.
 
-mv files/app_logrotate.conf /etc/logrotate.d/apps-logrotate.conf
+mv files/apps-logrotate.conf /etc/logrotate.d/apps-logrotate.conf
