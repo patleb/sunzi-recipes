4bca8671d1fbf51870f3d97ee0852185eef50a20
Wayne Schroer <wayne@metabahn.com>
2016-05-27 10:40:21 -0500

    Ensure curl is installed

--- longview.sh
+++ longview.sh
@@ -1,1 +1,7 @@
+if aptitude search '~i ^curl$' | grep -q curl; then
+  echo "curl already installed, skipping."
+else
+  apt-get -y install curl
+fi
+
 curl -s "https://lv.linode.com/$(cat attributes/longview_key)" | sudo bash
