db827d1e4425b1eb725906d289906c63cb68d028
Wayne Schroer <wayne@metabahn.com>
2016-05-27 10:37:27 -0500

    We need software-properties-common for 14.04

--- redis.sh
+++ redis.sh
@@ -1,7 +1,7 @@
 if aptitude search '~i ^redis-server$' | grep -q redis-server; then
   echo "redis-server already installed, skipping."
 else
-  apt-get -y install python-software-properties
+  apt-get -y install python-software-properties software-properties-common
   apt-add-repository ppa:chris-lea/redis-server
   apt-get update
 
