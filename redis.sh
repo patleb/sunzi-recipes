ca7dbefea6f0415584852ac0b37c8e6458e01347
Wayne Schroer <wayne@metabahn.com>
2016-05-27 10:34:19 -0500

    Add python-software-properties so we can use the apt-add-repository command

--- redis.sh
+++ redis.sh
@@ -1,6 +1,7 @@
 if aptitude search '~i ^redis-server$' | grep -q redis-server; then
   echo "redis-server already installed, skipping."
 else
+  apt-get install python-software-properties
   apt-add-repository ppa:chris-lea/redis-server
   apt-get update
 
