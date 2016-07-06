24820b6ce7f7a1e8543fd4fe8ae44997fbb123ce
Wayne Schroer <wayne@metabahn.com>
2016-07-06 14:19:52 -0500

    Special config for platform app nodes

---
+++ platform-app-nodes.sh
@@ -0,0 +1,4 @@
+# This file is for special configuration that applies only to a category of nodes.
+
+git config --global user.name "Pakyow Platform"
+git config --global user.email "admin@pakyow.com"
