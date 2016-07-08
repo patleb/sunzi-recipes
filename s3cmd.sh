84b3e9186eb90539125e28f629aab8d5affe9f6e
Wayne Schroer <wayne@metabahn.com>
2016-07-08 10:46:16 -0500

    Update the recipe to use the right file path and always copy the config

--- s3cmd.sh
+++ s3cmd.sh
@@ -2,6 +2,6 @@ if aptitude search '~i ^s3cmd$' | grep -q s3cmd; then
   echo "s3cmd already installed, skipping."
 else
   apt-get -y install s3cmd
-
-  mv ../files/.s3cfg /root/.s3cfg
 fi
+
+mv files/.s3cfg /root/.s3cfg
