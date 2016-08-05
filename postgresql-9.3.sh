b30b4d63a2da43e7f72f525aa5223d9b2d71447a
Wayne Schroer <wayne@metabahn.com>
2016-08-05 09:47:16 -0500

    Update the postgres9.3 recipe to be a bit more fault tolerant

--- postgresql-9.3.sh
+++ postgresql-9.3.sh
@@ -1,3 +1,5 @@
+private_ip=$(cat attributes/private_ip)
+
 if aptitude search '~i ^postgresql-9.3$' | grep -q postgresql-9.3; then
   echo "postgresql-9.3 already installed, skipping."
 else
@@ -7,22 +9,21 @@ else
   apt-get update
 
   apt-get -y install postgresql-9.3 postgresql-server-dev-9.3 postgresql-contrib-9.3
-  
-  private_ip=$(cat attributes/private_ip)
+fi
 
-  if [ ! -z "$private_ip" ]; then
-    # bind on the private ip
-    file='/etc/postgresql/9.3/main/postgresql.conf'
-    grep -q "^listen_addresses =" $file && sed "s/^listen_addresses = .*/listen_addresses = '$private_ip'/" -i $file || 
-      sed "$ a\listen_addresses = '$private_ip'" -i $file
+if [ ! -z "$private_ip" ]; then
+  # bind on the private ip
+  file='/etc/postgresql/9.3/main/postgresql.conf'
+  grep -q "^listen_addresses =" $file && sed "s/^listen_addresses = .*/listen_addresses = '$private_ip'/" -i $file || 
+    sed "$ a\listen_addresses = '$private_ip'" -i $file
 
-    # allow access
-    file='/etc/postgresql/9.3/main/pg_hba.conf'
-    conf='host    all             all             all                     md5'
-    if [ ! grep -q "^$conf" $file]; then
-      sed "$ a\$conf" -i $file
-    fi
-
-    service postgresql restart
+  # allow access
+  file='/etc/postgresql/9.3/main/pg_hba.conf'
+  conf='host    all             all             all                     md5'
+  if [ ! grep -q "^$conf" $file]; then
+    sed "$ a\$conf" -i $file
   fi
 fi
+
+# Always Restart
+service postgresql restart
