114fed6745501617a8a7f2cb4507f69eb316d540
Wayne Schroer <wayne@metabahn.com>
2016-08-05 15:11:59 -0500

    Handle longview a bit better, it breaks execution when it fails

--- longview.sh
+++ longview.sh
@@ -4,5 +4,10 @@ else
   apt-get -y install curl
 fi
 
-echo "https://lv.linode.com/$(cat attributes/longview_key)"
-curl -s "https://lv.linode.com/$(cat attributes/longview_key)" | sudo bash
+longview_key = $(cat attributes/longview_key)
+if grep -Fxq "$longview_key" "/etc/linode/longview.key"; then
+  echo "Longview is setup with the correct key, skipping installation"
+else
+  echo "Installing longview from https://lv.linode.com/$(cat attributes/longview_key)"
+  curl -s "https://lv.linode.com/$(cat attributes/longview_key)" | sudo bash
+fi
