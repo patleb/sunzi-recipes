1985bad3d30a03b751abe9a061dbc4e132698493
Wayne Schroer <wayne@metabahn.com>
2016-08-05 09:20:13 -0500

    Copypasta mistake

--- verify-hostname.sh
+++ verify-hostname.sh
@@ -2,8 +2,8 @@ hostname=$(cat attributes/hostname)
 
 if [ -e /etc/hostname-should-verify ]
 then
-  echo "hostname set; comparing value $ruby_version to node"
-  grep "^$ruby_version$" /etc/hostname || exit 1
+  echo "hostname set; comparing value $hostname to node"
+  grep "^$hostname$" /etc/hostname || exit 1
 else
   echo 'hostname has not been set by us; allowing access this once'
   touch /etc/hostname-should-verify
