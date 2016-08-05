2fd280aea9e8e5e543d30d840fed5dfefbf8c34b
Wayne Schroer <wayne@metabahn.com>
2016-08-05 09:15:15 -0500

    Add some info for debugging

--- verify-hostname.sh
+++ verify-hostname.sh
@@ -1,5 +1,6 @@
 if [ -e /etc/hostname-should-verify ]
 then
+  echo "hostname set; comparing value <%= @attributes.hostname %> to node"
   grep "^<%= @attributes.hostname %>$" /etc/hostname || exit 1
 else
   echo 'hostname has not been set by us; allowing access this once'
