12c1daaf6efae9546bf290a1ead023d05d9ec02e
Dan McGuire <dan@metabahn.com>
2016-08-10 16:10:23 -0500

    adds inspeqtor recipe

--- inspeqtor.sh
+++ inspeqtor.sh
@@ -1,6 +1,15 @@
-if [ -e /etc/inspeqtor/inspeqtor.conf ] then
+if [ -e /etc/inspeqtor/inspeqtor.conf ]; then
   echo "Inspeqtor already installed, skipping."
 else
+  echo "Installing inspeqtor."
   curl -L https://bit.ly/InspeqtorDEB | bash # add inspeqtor repo
   apt-get install inspeqtor
+fi
+
+if pgrep "inspeqtor" > /dev/null; then
+  echo "Restarting inspeqtor."
+  initctl restart inspeqtor
+else
+  echo "Starting inspeqtor."
+  initctl start inspeqtor
 fi
