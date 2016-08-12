8c51b71dc2d181a0daf6a0d4830d5cf98b9a3ec1
Wayne Schroer <wayne@metabahn.com>
2016-08-12 16:14:55 -0500

    Linode has their own kernel. :rage:

--- utilities.sh
+++ utilities.sh
@@ -16,6 +16,5 @@ else
   curl -s https://s3.amazonaws.com/download.draios.com/DRAIOS-GPG-KEY.public | apt-key add -
   curl -s -o /etc/apt/sources.list.d/draios.list http://download.draios.com/stable/deb/draios.list
   apt-get update
-  apt-get -y install linux-headers-$(uname -r)
   apt-get -y install sysdig
 fi
