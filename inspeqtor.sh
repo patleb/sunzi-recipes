c12fb1dcbb677a7be739a71496e8c33a62243848
Dan McGuire <dan@metabahn.com>
2016-08-12 14:02:10 -0500

    merge in master

---
+++ inspeqtor.sh
@@ -0,0 +1,17 @@
+if [ -e /etc/inspeqtor/inspeqtor.conf ]; then
+  echo "Inspeqtor already installed, skipping."
+else
+  echo "Installing inspeqtor."
+  apt install -y curl
+  curl -L https://bit.ly/InspeqtorDEB | bash # add inspeqtor repo
+  apt-get install inspeqtor
+  mkdir -p /etc/inspeqtor/services.d/
+fi
+
+if pgrep "inspeqtor" > /dev/null; then
+  echo "Restarting inspeqtor."
+  initctl restart inspeqtor
+else
+  echo "Starting inspeqtor."
+  initctl start inspeqtor
+fi
