10a0aa46091f6290d85eb236ff2b8754fb707d4f
Dan McGuire <dan@metabahn.com>
2016-08-12 14:02:42 -0500

    tweaks postgres install to use upstart

--- postgresql-9.5.sh
+++ postgresql-9.5.sh
@@ -1,9 +1,7 @@
-restart=false
 if aptitude search '~i ^postgresql-9.5$' | grep -q postgresql-9.5; then
   echo "postgresql-9.5 already installed, skipping."
 else
   echo "installing postgres"
-  restart=true
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
   sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
   apt-get update
@@ -14,8 +12,6 @@ fi
 if cmp -s files/postgresql.conf /etc/postgresql/9.5/main/postgresql.conf; then
   echo "postgresql.conf file is identical, skipping copy"
 else
-  restart=true
-
   # Install configuration
   rm /etc/postgresql/9.5/main/postgresql.conf
   mv files/postgresql.conf /etc/postgresql/9.5/main/postgresql.conf
@@ -25,18 +21,24 @@ fi
 if cmp -s files/pg_hba.conf /etc/postgresql/9.5/main/pg_hba.conf; then
   echo "pg_hba.conf file is identical, skipping copy"
 else
-  restart=true
-
   # Enable client authentication
   rm /etc/postgresql/9.5/main/pg_hba.conf
   mv files/pg_hba.conf /etc/postgresql/9.5/main/pg_hba.conf
   chown postgres /etc/postgresql/9.5/main/pg_hba.conf
 fi
+# disable initd and add upstart supervisor config
+update-rc.d postgresql disable
+mv files/pg_versions_upstart.conf /etc/init/pg_versions.conf
+mv files/pg_cluster_upstart.conf /etc/init/pg_cluster.conf
+mv files/pg_agent_upstart.conf /etc/init/pg_agent.conf
+#inspeqtor config
+mv files/pg_inspeqtor.inq /etc/inspeqtor/services.d/postgres.inq
+initctl reload-configuration
 
-if [ $restart = true ]; then
-  /etc/init.d/postgresql restart
+if pgrep "postgres" > /dev/null; then
+  echo "Restarting postgres."
+  restart pg_versions
 else
-  echo "No update, skipping postgres restart"
+  echo "Starting postgres."
+  start pg_versions
 fi
-
-
