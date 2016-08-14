03c308c499225ab61d550bb82bd6b2219a50fd3f
Dan McGuire <dan@metabahn.com>
2016-08-14 18:15:03 -0500

    --wip--

--- nginx-config.sh
+++ nginx-config.sh
@@ -1,8 +1,8 @@
 rm -f /etc/nginx/nginx.conf
-rm -f /etc/logrotate.d/nginx
+#rm -f /etc/logrotate.d/nginx
 
 mv files/nginx.conf /etc/nginx/nginx.conf
-mv files/nginx-logrotate.conf /etc/logrotate.d/nginx
+#mv files/nginx-logrotate.conf /etc/logrotate.d/nginx
 
 chown root /etc/nginx/nginx.conf
 chgrp root /etc/nginx/nginx.conf
