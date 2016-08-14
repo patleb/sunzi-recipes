ee2453bd9aceb9aa7f3171c63545c7d12a692656
Wayne Schroer <wayne@metabahn.com>
2016-08-14 12:18:23 -0500

    Add fail2ban so secure ssh against abuse

---
+++ fail2ban.sh
@@ -0,0 +1,20 @@
+if aptitude search '~i ^fail2ban$' | grep -q fail2ban; then
+  echo "fail2ban already installed, skipping."
+else
+  apt-get -y install fail2ban
+fi
+
+cat >/etc/fail2ban/fail2ban.local <<EOL
+[Definition]
+
+loglevel = 3
+logtarget = /var/log/fail2ban.log
+et = /var/run/fail2ban/fail2ban.sock
+pidfile = /var/run/fail2ban/fail2ban.pid
+EOL
+
+cat >/etc/fail2ban/jail.local <<EOL
+# Edit default settings to jails here.
+EOL
+
+fail2ban-client reload
