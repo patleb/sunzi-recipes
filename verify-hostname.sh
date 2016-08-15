66b0c0c54896dbb14d052c80df3b3d8515505ff6
Wayne Schroer <wayne@metabahn.com>
2016-08-14 21:15:39 -0500

    Print both value in the output

--- verify-hostname.sh
+++ verify-hostname.sh
@@ -1,8 +1,9 @@
 hostname=$(cat attributes/hostname)
+nodename=$(cat /etc/hostname)
 
 if [ -e /etc/hostname-should-verify ]
 then
-  echo "hostname set; comparing value $hostname to node"
+  echo "hostname set; comparing value $hostname to $nodename"
   grep "^$hostname$" /etc/hostname || exit 1
 else
   echo 'hostname has not been set by us; allowing access this once'
