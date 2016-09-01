7be59c8edc6ff46d688bc197527f91e4617a028f
Bryan Powell <bryan@metabahn.com>
2016-09-01 14:57:51 -0700

    Simplify networking

--- network-interfaces.sh
+++ network-interfaces.sh
@@ -18,22 +18,18 @@ iface lo inet loopback
 auto eth0 eth0:0
 
 iface eth0 inet static
- address <%= @attributes.public_ip %>
- netmask '255.255.255.0'
+ address <%= @attributes.public_ip %>/24
  gateway <%= @attributes.public_gateway %>
 
 <% if @attributes.private_ip %>
 iface eth0:0 inet static
- address <%= @attributes.private_ip %>
- netmask 255.255.128.0
+ address <%= @attributes.private_ip %>/17
 <% end %>
 
 <% if @attributes.failover_ip && @attributes.failover_gateway %>
-auto eth0:1
-iface eth0:1 inet static
-  address <%= @attributes.failover_ip %>
-  gateway <%= @attributes.failover_gateway %>
-  netmask 255.255.255.0
+iface eth0:0 inet static
+ address <%= @attributes.failover_ip %>/24
+ gateway <%= @attributes.failover_gateway %>
 <% end %>
 EOT
 
