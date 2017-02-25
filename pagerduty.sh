f6079bacec5f56a710d3493e19f41dd949c4c6ef
Bryan Powell <bryan@metabahn.com>
2017-02-25 15:12:56 -0800

    Update pagerduty.sh

--- pagerduty.sh
+++ pagerduty.sh
@@ -1,7 +1,8 @@
 if aptitude search '~i ^pdagent$' | grep -q pdagent; then
   echo "pdagent already installed, skipping."
-else
-  wget -O - http://packages.pagerduty.com/GPG-KEY-pagerduty | sudo apt-key add -
+else  
+  wget -O - https://packages.pagerduty.com/GPG-KEY-pagerduty | sudo apt-key add -
+  sh -c 'echo "deb https://packages.pagerduty.com/pdagent deb/" >/etc/apt/sources.list.d/pdagent.list'
   apt-get update
   apt-get -y install pdagent pdagent-integrations
 fi
